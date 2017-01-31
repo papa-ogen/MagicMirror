5db50b2bbcdfb4bd689d193dfb77dfbb2d8861a5
Thelin <K320940-ADM@oneadr.net>
2017-01-31 14:35:35 +0100

    Removed fonts. Started TypeScript conversion of previous code

--- dist/js/magicmirror.js
+++ dist/js/magicmirror.js
@@ -1,4 +1,43 @@
 var mmConfig = {};
 var test = "mmCalendar";
-(function (config) {
-})(mmConfig);
+var mmClock = (function () {
+    function mmClock() {
+    }
+    mmClock.prototype.Time = function () {
+        var _this = this;
+        var d = new Date();
+        var hours = d.getHours().toString();
+        var minutes = d.getMinutes().toString();
+        setTimeout(function () { _this.Time(); }, 1000);
+        if (hours.length === 1) {
+            hours = "0" + hours;
+        }
+        if (minutes.length === 1) {
+            minutes = "0" + minutes;
+        }
+        document.querySelector(".mm-hour").textContent = hours;
+        document.querySelector(".mm-minute").textContent = minutes;
+    };
+    return mmClock;
+})();
+var mmDate = (function () {
+    function mmDate() {
+        this.weekDays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
+        var d = new Date();
+        var weekDay = d.getDay();
+        this.weekDay = this.weekDays[weekDay];
+    }
+    mmDate.prototype.getWeekDay = function () {
+        var d = new Date();
+        var weekDay = d.getDay();
+        this.weekDay = this.weekDays[weekDay];
+        return this.weekDay;
+    };
+    return mmDate;
+})();
+window.onload = function () {
+    var clock = new mmClock();
+    clock.Time();
+    var mmDate = new mmDate();
+    // console.log(mmDate.getWeekDay());
+};
--- src/ts/mmClock.ts
+++ src/ts/mmClock.ts
@@ -1,3 +1,48 @@
-(function(config) {
+class mmClock {
+     Time() {
+        var d = new Date();
+        var hours = d.getHours().toString();
+        var minutes = d.getMinutes().toString();
+        
+        setTimeout(() => { this.Time(); }, 1000);
+        
+        if (hours.length === 1) {
+            hours = "0" + hours;
+        }
 
-})(mmConfig);
+        if (minutes.length === 1) {
+            minutes = "0" + minutes;
+        }
+        
+        document.querySelector(".mm-hour").textContent = hours;
+        document.querySelector(".mm-minute").textContent = minutes;
+    }
+}
+
+
+class mmDate {
+     weekDays: string[] = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
+     weekDay: string;
+
+    constructor() {
+        var d = new Date();
+        var weekDay = d.getDay();
+        this.weekDay = this.weekDays[weekDay];
+    }
+
+    getWeekDay() {
+        var d = new Date();
+        var weekDay = d.getDay();
+        this.weekDay = this.weekDays[weekDay];        
+        return this.weekDay;
+    }
+}
+
+window.onload = () => {
+    var clock = new mmClock();
+    clock.Time();
+
+    var mmDate = new mmDate();
+
+    // console.log(mmDate.getWeekDay());
+};
