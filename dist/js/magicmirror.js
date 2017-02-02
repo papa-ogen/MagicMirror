8cc2ef80ccdfcb8dea0222301de26415f3116810
Thelin <K320940-ADM@oneadr.net>
2017-02-02 09:20:16 +0100

    bug fix on cache for names. Started on weather

--- dist/js/magicmirror.js
+++ dist/js/magicmirror.js
@@ -12,90 +12,113 @@ var mmHelper = {
         updateInterval: 60000
     }
 };
-var mmClock = (function () {
-    function mmClock() {
-    }
-    mmClock.prototype.Time = function () {
-        var _this = this;
-        var d = new Date();
-        var hours = d.getHours().toString();
-        var minutes = d.getMinutes().toString();
-        setTimeout(function () { _this.Time(); }, 1000);
-        if (hours.length === 1) {
-            hours = "0" + hours;
+var mmClock;
+(function (mmClock) {
+    var Clock = (function () {
+        function Clock() {
         }
-        if (minutes.length === 1) {
-            minutes = "0" + minutes;
-        }
-        document.querySelector(".mm-hour").textContent = hours;
-        document.querySelector(".mm-minute").textContent = minutes;
-    };
-    return mmClock;
-})();
-var mmWeekDay = (function () {
-    function mmWeekDay() {
-        this.weekDays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
-        var d = new Date();
-        var weekDay = d.getDay();
-        this.weekDay = this.weekDays[weekDay];
-    }
-    mmWeekDay.prototype.getWeekDay = function () {
-        return this.weekDay;
-    };
-    return mmWeekDay;
-})();
-var mmDate = (function () {
-    function mmDate(d) {
-        this.months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni",
-            "Juli", "Augusti", "September", "Oktober", "November", "December"];
-        this.currentMonth = d.getMonth();
-        this.currentDate = d.getDate();
-    }
-    mmDate.prototype.getCurrentDate = function () {
-        return this.months[this.currentMonth] + " " + this.currentDate;
-    };
-    return mmDate;
-})();
-var mmNames = (function () {
-    function mmNames(url) {
-        this.apiUrl = url;
-    }
-    mmNames.prototype.getJSON = function (callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("get", this.apiUrl, true);
-        xhr.responseType = "json";
-        xhr.onload = function () {
-            var status = xhr.status;
-            if (status == 200) {
-                callback(null, xhr.response);
+        Clock.prototype.Time = function () {
+            var _this = this;
+            var d = new Date();
+            var hours = d.getHours().toString();
+            var minutes = d.getMinutes().toString();
+            setTimeout(function () { _this.Time(); }, 1000);
+            if (hours.length === 1) {
+                hours = "0" + hours;
             }
-            else {
-                callback(status);
+            if (minutes.length === 1) {
+                minutes = "0" + minutes;
             }
+            document.querySelector(".mm-hour").textContent = hours;
+            document.querySelector(".mm-minute").textContent = minutes;
         };
-        xhr.send();
-    };
-    mmNames.prototype.getNamesDayNames = function (data) {
-        var namesDayNames = data.dagar[0].namnsdag;
-        return namesDayNames.join(", ");
-    };
-    mmNames.prototype.addNames = function () {
-        var _this = this;
-        this.getJSON(function (err, data) {
-            if (err != null) {
-                console.log("Something went wrong: " + err);
-            }
-            else {
-                document.querySelector(".mm-names").textContent = _this.getNamesDayNames(data);
-            }
-        });
-    };
-    return mmNames;
-})();
-var clock = new mmClock();
-clock.Time();
-var weekday = new mmWeekDay();
-var curDate = new mmDate(new Date());
-document.querySelector(".mm-datetime").textContent = weekday.getWeekDay() + ", " + curDate.getCurrentDate();
-var names = new mmNames("http://api.dryg.net/dagar/v2.1/");
-names.addNames();
+        return Clock;
+    })();
+    var WeekDay = (function () {
+        function WeekDay() {
+            this.weekDays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
+            var d = new Date();
+            var weekDay = d.getDay();
+            this.weekDay = this.weekDays[weekDay];
+        }
+        WeekDay.prototype.getWeekDay = function () {
+            return this.weekDay;
+        };
+        return WeekDay;
+    })();
+    var MonthDate = (function () {
+        function MonthDate(d) {
+            this.months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni",
+                "Juli", "Augusti", "September", "Oktober", "November", "December"];
+            this.currentMonth = d.getMonth();
+            this.currentDate = d.getDate();
+        }
+        MonthDate.prototype.getCurrentDate = function () {
+            return this.months[this.currentMonth] + " " + this.currentDate;
+        };
+        return MonthDate;
+    })();
+    var Names = (function () {
+        function Names(helper) {
+            this.apiUrl = helper.apiUrl + '?nocache=' + (new Date()).getTime();
+            this.updateInterval = helper.updateInterval;
+        }
+        Names.prototype.getJSON = function (callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("get", this.apiUrl, true);
+            xhr.responseType = "json";
+            xhr.onload = function () {
+                var status = xhr.status;
+                if (status == 200) {
+                    callback(null, xhr.response);
+                }
+                else {
+                    callback(status);
+                }
+            };
+            xhr.send();
+        };
+        Names.prototype.addNames = function () {
+            var _this = this;
+            this.getJSON(function (err, data) {
+                if (err != null) {
+                    console.log("Something went wrong: " + err);
+                }
+                else {
+                    console.log(data);
+                    document.querySelector(".mm-names").textContent = data.dagar[0].namnsdag.join(", ");
+                }
+            });
+            setTimeout(function () { _this.addNames(); }, 360000);
+        };
+        return Names;
+    })();
+    var clock = new Clock();
+    clock.Time();
+    var weekday = new WeekDay();
+    var curDate = new MonthDate(new Date());
+    document.querySelector(".mm-datetime").textContent = weekday.getWeekDay() + ", " + curDate.getCurrentDate();
+    var names = new Names(mmHelper.mmClock);
+    names.addNames();
+})(mmClock || (mmClock = {}));
+var mmWeather;
+(function (mmWeather) {
+    var mm = mmHelper;
+    var weatherContainer = document.querySelector(".mm-weather");
+    var Weather = (function () {
+        function Weather(data) {
+            this.name = data.name !== "undefined" ? data.name : "";
+            this.weather = data.weather[0].main;
+            this.description = data.weather[0].description;
+            this.icon = data.weather[0].icon;
+            this.temp = (data.main.temp - 273.15).toFixed(2);
+        }
+        Weather.prototype.getTime = function (data) {
+            var d = data.dt_txt !== "undefined" ? data.dt_txt.replace(/\s/g, "T") : "";
+            if (d.length > 0)
+                return new Date(d).getDay();
+            return;
+        };
+        return Weather;
+    })();
+})(mmWeather || (mmWeather = {}));
--- src/ts/mmClock.ts
+++ src/ts/mmClock.ts
@@ -1,111 +1,106 @@
-class mmClock {
-    Time() {
-        var d = new Date();
-        var hours = d.getHours().toString();
-        var minutes = d.getMinutes().toString();
-        
-        setTimeout(() => { this.Time(); }, 1000);
-        
-        if (hours.length === 1) {
-            hours = "0" + hours;
+namespace mmClock {
+    class Clock {
+        Time() {
+            var d = new Date();
+            var hours = d.getHours().toString();
+            var minutes = d.getMinutes().toString();
+            
+            setTimeout(() => { this.Time(); }, 1000);
+            
+            if (hours.length === 1) {
+                hours = "0" + hours;
+            }
+
+            if (minutes.length === 1) {
+                minutes = "0" + minutes;
+            }
+            
+            document.querySelector(".mm-hour").textContent = hours;
+            document.querySelector(".mm-minute").textContent = minutes;
+        }
+    }
+
+    class WeekDay {
+        weekDays: string[] = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
+        weekDay: string;
+
+        constructor() {
+            let d = new Date();
+            let weekDay = d.getDay();
+            this.weekDay = this.weekDays[weekDay];
         }
 
-        if (minutes.length === 1) {
-            minutes = "0" + minutes;
+        getWeekDay() {
+            return this.weekDay;
         }
+    }
+
+    class MonthDate {
+        months: string[] = ["Januari", "Februari", "Mars", "April", "Maj", "Juni",
+        "Juli", "Augusti", "September", "Oktober", "November", "December"];
+        currentMonth: number;
+        currentDate: number;
+
+        constructor(d: Date) {
+            this.currentMonth = d.getMonth();
+            this.currentDate = d.getDate();
+
+        }
+
+        getCurrentDate() {
+            return this.months[this.currentMonth] + " " + this.currentDate;
+        }
+    }
+
+    class Names {
+        apiUrl: string;
+        updateInterval: number;
         
-        document.querySelector(".mm-hour").textContent = hours;
-        document.querySelector(".mm-minute").textContent = minutes;
+        constructor(helper) {
+            this.apiUrl = helper.apiUrl + '?nocache=' + (new Date()).getTime();
+            this.updateInterval = helper.updateInterval
+        }
+
+        getJSON(callback) {
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("get", this.apiUrl, true);
+            xhr.responseType = "json";
+            xhr.onload = function() {
+                var status = xhr.status;
+                if (status == 200) {
+                    callback(null, xhr.response);
+                } else {
+                    callback(status);
+                }
+            };
+
+            xhr.send();
+        }
+
+        addNames() {
+            this.getJSON(function(err, data) {
+                if (err != null) {
+                    console.log("Something went wrong: " + err);
+                } else {
+                    console.log(data);
+                    document.querySelector(".mm-names").textContent = data.dagar[0].namnsdag.join(", ");
+                }
+            });
+
+            setTimeout(() => { this.addNames(); }, 360000);
+        }
     }
+
+    var clock = new Clock();
+    clock.Time();
+
+    var weekday = new WeekDay();
+
+    var curDate = new MonthDate(new Date());
+
+    document.querySelector(".mm-datetime").textContent = weekday.getWeekDay() + ", " + curDate.getCurrentDate();
+
+    var names = new Names(mmHelper.mmClock);
+    names.addNames();
 }
-
-class mmWeekDay {
-    weekDays: string[] = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Saturday", "Sunday"];
-    weekDay: string;
-
-    constructor() {
-        let d = new Date();
-        let weekDay = d.getDay();
-        this.weekDay = this.weekDays[weekDay];
-    }
-
-    getWeekDay() {
-        return this.weekDay;
-    }
-}
-
-class mmDate {
-    months: string[] = ["Januari", "Februari", "Mars", "April", "Maj", "Juni",
-    "Juli", "Augusti", "September", "Oktober", "November", "December"];
-    currentMonth: number;
-    currentDate: number;
-
-    constructor(d: Date) {
-        this.currentMonth = d.getMonth();
-        this.currentDate = d.getDate();
-
-    }
-
-    getCurrentDate() {
-        return this.months[this.currentMonth] + " " + this.currentDate;
-    }
-}
-
-interface Name {
-  name: string;
-}
-
-class mmNames {
-    apiUrl: string;
-    
-    constructor(url: string) {
-        this.apiUrl = url;
-    }
-
-    getJSON(callback) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open("get", this.apiUrl, true);
-        xhr.responseType = "json";
-        xhr.onload = function() {
-            var status = xhr.status;
-            if (status == 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(status);
-            }
-        };
-
-        xhr.send();
-    }
-
-    getNamesDayNames(data) {
-        let namesDayNames = data.dagar[0].namnsdag;
-
-        return namesDayNames.join(", ");
-    }    
-
-    addNames() {
-        let _this = this;
-
-        this.getJSON(function(err, data) {
-            if (err != null) {
-                console.log("Something went wrong: " + err);
-            } else {
-                document.querySelector(".mm-names").textContent = _this.getNamesDayNames(data);
-            }
-        });
-    }
-}
-
-var clock = new mmClock();
-clock.Time();
-
-var weekday = new mmWeekDay();
-
-var curDate = new mmDate(new Date());
-
-document.querySelector(".mm-datetime").textContent = weekday.getWeekDay() + ", " + curDate.getCurrentDate();
-
-var names = new mmNames("http://api.dryg.net/dagar/v2.1/");
-names.addNames();
