f25ff1acbcc77f44bc531c0386b4a64b945f59b0
Jörgen Thelin <thelin.jorgen@gmail.com>
2017-02-07 21:04:28 +0100

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,9 @@
-
+var mmConfig = {
+    mmWeather: {
+        apiKey: "fe4dabf01ceee9ab572ed61b924957c3",
+		lat: "59.3833",
+		long: "17.8333",
+		updateInterval: 60000,
+		foreCast: false
+    }
+};
