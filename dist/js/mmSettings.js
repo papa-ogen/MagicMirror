b9d157b9aecab265eebf402087657b15a37ff83d
Thelin <K320940-ADM@oneadr.net>
2017-02-09 12:40:40 +0100

    Changed settings name

---
+++ dist/js/mmSettings.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var mmHelper = {
+	mmCalendar: {
+		// CLIENT_ID: _config.googleApiKey,
+		SCOPES: ["https://www.googleapis.com/auth/calendar.readonly"]
+	},
+	mmWeather: {
+		apiUrl: "http://api.openweathermap.org/data/2.5/weather",
+		forecastUrl: "http://api.openweathermap.org/data/2.5/forecast",
+		apiId: mmConfig.mmWeather.apiKey,
+		lat: mmConfig.mmWeather.lat,
+		lon: mmConfig.mmWeather.long,
+		updateInterval: mmConfig.mmWeather.updateInterval,
+		foreCast: mmConfig.mmWeather.foreCast
+	},
+	mmClock: {
+		apiUrl: "http://api.dryg.net/dagar/v2.1/",
+		updateInterval: 60000
+	},
+	getJSON: function getJSON(apiUrl, callback) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.open("get", apiUrl, true);
+		xhr.responseType = "json";
+		xhr.onload = function () {
+			var status = xhr.status;
+			if (status == 200) {
+				callback(null, xhr.response);
+			} else {
+				callback(status);
+			}
+		};
+
+		xhr.send();
+	}
+};
+//# sourceMappingURL=mmSettings.js.map
