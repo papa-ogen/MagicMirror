ae6b345aab26e6f0befabdb9aa695898759add4a
Thelin <K320940-ADM@oneadr.net>
2017-02-09 12:59:14 +0100

    Animation broken clouds

--- data/config.js
+++ data/config.js
@@ -3,7 +3,7 @@ var mmConfig = {
         apiKey: "fe4dabf01ceee9ab572ed61b924957c3",
 		lat: "59.3833",
 		long: "17.8333",
-		updateInterval: 60000,
+		updateInterval: 360000,
 		foreCast: false
     }
 };
