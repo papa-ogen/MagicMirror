fab0cc0fa822ba7102a777a517dd19b6e67b470c
Jörgen Thelin <thelin.jorgen@gmail.com>
2023-02-26 14:02:42 +0100

    adding craven ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button } from "@papa-ogen/craven-ui";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Magic Mirror</h1>
+      <Button text="Hello World" />
     </div>
   );
 }
